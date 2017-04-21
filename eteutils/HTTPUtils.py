96e3d06bc97c9f357e5a74e3bee2cb6f509fadee
Jerry Flood <jf9860@att.com>
2017-04-21 16:32:33 -0400

    [TEST-18] Add function to suppress cert warnings
    Change-Id: I089debc2d8b736316443a53375a15e4791bef9bd
    Signed-off-by: Jerry Flood <jf9860@att.com>

--- eteutils/HTTPUtils.py
+++ eteutils/HTTPUtils.py
@@ -1,8 +1,13 @@
 import urllib
+from requests.packages import urllib3
 
 class HTTPUtils:
     """HTTPUtils is common resource for simple http helper keywords."""
-    
+
     def url_encode_string(self, barestring):
         """URL Encode String takes in a string and converts into 'percent-encoded' string"""
-        return urllib.quote_plus(barestring) 
+        return urllib.quote_plus(barestring)
+
+    def disable_warnings(self):
+        """  Disable the cert warnings when creating sessions for A&AI API Calls """
+        urllib3.disable_warnings()
