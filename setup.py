f64f67987e57e6beda2514612ab4933a4950f885
Brian Freeman <bf1936@att.com>
2019-03-20 15:39:16 -0500

    Fix for deepdiff 4.0 upgrade
    deepdiff upgraded to 4.0 in latest and doesn't work with Python2.7
    Need to specify a deepdif before 3.3
    
    Issue-ID: INT-990
    Change-Id: I681e3663e5b1d87d36d889f1cd8be1f326ed7e57
    Signed-off-by: Brian Freeman <bf1936@att.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='python-openecomp-eteutils',            # This is the name of your PyPI-package.
     version='0.2',                          # Update the version number for new releases
     description='Scripts written to be used during ete testing',    # Info about script
-    install_requires=['dnspython','paramiko', 'pyyaml', 'robotframework', 'deepdiff'], # what we need
+    install_requires=['dnspython','paramiko', 'pyyaml', 'robotframework', 'deepdiff>=2.5,<3.3'], # what we need
     packages=['eteutils', 'loadtest'],       # The name of your scipts package
     package_dir={'eteutils': 'eteutils', 'loadtest' : 'loadtest'} # The location of your scipts package
 )
