dea8765ee75fb4b4c402d02afb1c53c8116e2176
DR695H <dr695h@att.com>
2019-05-24 16:33:23 -0400

    not ready to submit my changes to soutils yet
    Change-Id: I02122ad3fcf8d32efaed6976246943902f112e44
    Issue-ID: TEST-158
    Signed-off-by: DR695H <dr695h@att.com>

--- robotframework-onap/ONAPLibrary/RequestSOKeywords.py
+++ robotframework-onap/ONAPLibrary/RequestSOKeywords.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from BaseSOKeywords import BaseSOKeywords
+from ONAPLibrary.BaseSOKeywords import BaseSOKeywords
 from robot.api import logger
 from robot.api.deco import keyword
 from robot.libraries.BuiltIn import BuiltIn
