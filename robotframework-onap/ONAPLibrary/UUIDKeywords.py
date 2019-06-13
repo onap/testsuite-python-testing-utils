d8683928e802b01956548ed384ef895c92d52949
DR695H <dr695h@att.com>
2019-06-13 14:40:27 -0400

    move dns and uuid to the new format of keywords
    Change-Id: I07612b85424c4d687b0c551ecc7727920d2736b0
    Issue-ID: TEST-164
    Signed-off-by: DR695H <dr695h@att.com>

---
+++ robotframework-onap/ONAPLibrary/UUIDKeywords.py
@@ -0,0 +1,27 @@
+import uuid
+import time
+import datetime
+from robot.api.deco import keyword
+
+
+class UUIDKeywords(object):
+    """ Utilities useful for generating UUIDs """
+
+    def __init__(self):
+        super(UUIDKeywords, self).__init__()
+
+    @keyword
+    def generate_uuid4(self):
+        """generate a uuid"""
+        return uuid.uuid4()
+
+    @keyword
+    def generate_uuid1(self):
+        """generate a timestamp uuid"""
+        return uuid.uuid1()
+
+    @keyword
+    def generate_timestamp(self):
+        """generate a timestamp"""
+        then = datetime.datetime.now()
+        return int(time.mktime(then.timetuple()) * 1e3 + then.microsecond / 1e3)
--- robotframework-onap/ONAPLibrary/Utilities.py
+++ robotframework-onap/ONAPLibrary/Utilities.py
@@ -14,13 +14,15 @@
 
 from ONAPLibrary.robotlibcore import HybridCore
 from ONAPLibrary.DNSKeywords import DNSKeywords
+from ONAPLibrary.UUIDKeywords import UUIDKeywords
 
 
-class DNS(HybridCore):
+class Utilities(HybridCore):
     """ DNS Keywords are useful for DNS requests """
 
     def __init__(self):
         self.keyword_implementors = [
-            DNSKeywords()
+            DNSKeywords(),
+            UUIDKeywords()
         ]
         HybridCore.__init__(self, self.keyword_implementors)
