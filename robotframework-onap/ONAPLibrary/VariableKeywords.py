7ff36c189f1418b54bf229e570417e63e8d42739
DR695H <dr695h@att.com>
2019-07-31 17:42:10 -0400

    pull global vars from robot trial
    Issue-ID: TEST-184
    Change-Id: I008c9cfbc1ae65c7735fcab2b474a0cbf7ec2ca9
    Signed-off-by: DR695H <dr695h@att.com>

--- robotframework-onap/ONAPLibrary/VariableKeywords.py
+++ robotframework-onap/ONAPLibrary/VariableKeywords.py
@@ -13,29 +13,32 @@
 # limitations under the License.
 
 from robot.libraries.BuiltIn import BuiltIn
+from robot.api.deco import keyword
 
 
-class VariableHelper(object):
-    """ Non keyword class for useful for working with varaibles """
+class VariableKeywords(object):
+    """ keyword class for useful keywords for working with varaibles """
 
     def __init__(self):
-        super(VariableHelper, self).__init__()
+        super(VariableKeywords, self).__init__()
         self.builtin = BuiltIn()
 
+    @keyword
     def get_globally_injected_parameters(self):
         dictionary = self.builtin.get_variables(no_decoration=True)
-        return self.filter_variables_by_key_prefix(dictionary, "GLOBAL_INJECTED_")
+        return self._filter_variables_by_key_prefix(dictionary, "GLOBAL_INJECTED_")
 
+    @keyword
     def get_global_parameters(self):
         dictionary = self.builtin.get_variables(no_decoration=True)
-        global_variables = self.filter_variables_by_key_prefix(dictionary, "GLOBAL_")
+        global_variables = self._filter_variables_by_key_prefix(dictionary, "GLOBAL_")
         # strip out global injected (get those above)
         for key in self.get_globally_injected_parameters():
             del global_variables[key]
         return global_variables
 
     @staticmethod
-    def filter_variables_by_key_prefix(dictionary, partial):
+    def _filter_variables_by_key_prefix(dictionary, partial):
         matches = dict()
         for key, val in dictionary.items():
             if key.startswith(partial):
