a3cb27d7b4ea976b057430bc4d519039c545f638
marekpl <marek.pondel@nokia.com>
2019-08-05 16:17:22 +0200

    send_binary_data_over_ssl keyword added to SocketKeywords.py
    send_binary_data_over_ssl keyword added to SocketKeywords.py
    
    Issue-ID: DCAEGEN2-565
    Signed-off-by: marekpl <marek.pondel@nokia.com>
    Change-Id: Icdb36610a78f955f0ffe0db2ce6e23be9727401d

--- robotframework-onap/ONAPLibrary/SocketKeywords.py
+++ robotframework-onap/ONAPLibrary/SocketKeywords.py
@@ -1,4 +1,4 @@
-import socket
+import socket, ssl
 from robot.api.deco import keyword
 
 
@@ -16,3 +16,16 @@ class SocketKeywords(object):
         sock.connect((host, int(port)))
         sock.sendall(bytes(data))
         sock.close()
+
+    @keyword
+    def send_binary_data_over_ssl(self, host, port, ca, cert, key, data):
+        """ send raw bytes over tcp ssl socket """
+        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        context.verify_mode = ssl.CERT_REQUIRED
+        context.load_verify_locations(str(ca))
+        context.load_cert_chain(str(cert), str(key))
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ssock = context.wrap_socket(sock, server_hostname=str(host))
+        ssock.connect((str(host), int(port)))
+        ssock.sendall(bytes(data))
+        ssock.close()
